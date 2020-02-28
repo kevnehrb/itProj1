9825450baa5f80688e21698850fbf0eabadd242e
kevnehrb <48367725+kevnehrb@users.noreply.github.com>
2020-02-28 14:07:07 -0500

    Add files via upload

--- rs.py
+++ rs.py
@@ -1,1 +1,22 @@
 import socket as mysoc
+
+dns = []
+
+def createDict():
+	inputFile = open("PROJI-DNSTS.txt","r")
+	entries = inputFile.readlines()
+	for i in entries:
+		newEntry = i.split()
+		dns.append(newEntry)
+	inputFile.close()
+
+def lookUp(hostname):
+	found = False
+	for i in dns:
+		for j in i:
+			if hostname.lower() == j.lower():
+				found = True
+	if found:
+		print("its in there")
+	else:
+		print("doo doo feces")
--- ts.py
+++ ts.py
@@ -1,40 +1,46 @@
 import socket as mysoc
 
-
-
+dns = []
 
 def createDict():
-    dns = []
-    inputFile = open("PROJI-DNSTS.txt","r")
-    inputFile.readlines()
+	inputFile = open("PROJI-DNSTS.txt","r")
+	entries = inputFile.readlines()
+	for i in entries:
+		newEntry = i.split()
+		dns.append(newEntry)
+	inputFile.close()
 
-
-
-def lookUp(hostname, dns):
-    if hostname in dns:
-        #check flag and return value based on that
-    else:
-        #return error string
-
-
+def lookUp(hostname):
+	found = False
+	for i in dns:
+		for j in i:
+			if hostname.lower() == j.lower():
+				found = True
+	if found:
+		print("its in there")
+	else:
+		print("doo doo feces")
 
 def server():
-    try:
-        tss=mysoc.socket(mysoc.AF_INET, mysoc.SOCK_STREAM)
-        print("[C]: TS socket created")
-    except mysoc.error as err:
-        print('{} \n'.format("socket open error ",err))
+	try:
+		tss=mysoc.socket(mysoc.AF_INET, mysoc.SOCK_STREAM)
+		print("[C]: TS socket created")
+	except mysoc.error as err:
+		print('{} \n'.format("socket open error ",err))
 
-    server_binding=('',50007)
-    tss.bind(server_binding)
-    tss.listen(1)
-    host=mysoc.gethostname()
-    print("[S]: Server host name is: ",host)
-    localhost_ip=(mysoc.gethostbyname(host))
-    print("[S]: Server IP address is  ",localhost_ip)
-    csockid,addr=tss.accept()
-    print ("[S]: Got a connection request from a client at", addr)
+	server_binding=('',50007)
+	tss.bind(server_binding)
+	tss.listen(1)
+	host=mysoc.gethostname()
+	print("[S]: Server host name is: ",host)
+	localhost_ip=(mysoc.gethostbyname(host))
+	print("[S]: Server IP address is  ",localhost_ip)
+	csockid,addr=tss.accept()
+	print ("[S]: Got a connection request from a client at", addr)
+
+
 
 
 
 createDict()
+lookUp("www.rutgers.COM")
